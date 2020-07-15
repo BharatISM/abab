da8414c8ca150f2dd86c9bef3acd912295208c58
Rahul Kedia <kediarahul1011@gmail.com>
2020-07-15 18:53:31 +0530

    Add code to check if polygon's aruco markers is present
    This function checks if the polygon's aruco markers is present in the
    image. If yes then it returns true, else if updates the value of next
    aruco id and returns false.
    Then in the main function, Polygon's aruco is updated.

--- main.cpp
+++ main.cpp
@@ -14,8 +14,33 @@
 
 std::string InputVideoPath = "Videos/Aruco_Hand_Video.mp4";
 
+
+bool CkeckIfArucoVisible(int Polygon_ID, std::vector<int> IDs, int& Next_Polygon_ID)
+{
+	int Num_of_IDs = IDs.size();
+
+	for (int i = 0; i < Num_of_IDs; i++)
+	{
+		if (Polygon_ID == IDs[i])
+			return true;
+		else if (Polygon_ID > IDs[i])
+			continue;
+		else if (Polygon_ID < IDs[i])
+		{
+			Next_Polygon_ID = IDs[i];
+			return false;
+		}
+	}
+	Next_Polygon_ID = IDs[0];
+	return false;
+}
+
+
 int main()
 {
+	bool FirstFrameFlag = true;
+	int Polygon_ID;
+
 	cv::VideoCapture Cap(InputVideoPath);
 
 	// Check if camera opened successfully
@@ -36,7 +61,25 @@ int main()
 			break;
 		
 		std::vector<int> IDs; std::vector<std::vector<cv::Point2f>> Corners;
-		DetectAruco(Frame, IDs, Corners);
+		bool IsArucoFound = DetectAruco(Frame, IDs, Corners);
+
+		if (!IsArucoFound)
+			continue;
+
+		if (IsArucoFound && FirstFrameFlag)
+		{
+			FirstFrameFlag = false;
+			Polygon_ID = IDs[0];
+		}
+
+		int Next_Polygon_ID;
+		bool IsArucoVisible = CkeckIfArucoVisible(Polygon_ID, IDs, Next_Polygon_ID);
+
+		if (!IsArucoVisible)
+			Polygon_ID = Next_Polygon_ID;
+
+
+
 
 		cv::imshow("Input", Frame);
 		if (cv::waitKey(1) == 32)
