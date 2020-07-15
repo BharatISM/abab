aa5e6aa9fc77738da9cfa302b72021a6ec271d5b
Rahul Kedia <kediarahul1011@gmail.com>
2020-07-15 17:52:13 +0530

    Add main.cpp
    This file will be the main file of our project.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File name	: main.cpp
+ * Created by	: Rahul Kedia
+ * Created on	: 15/07/2020
+ * Description	: This file contains main code for the project.
+ */
+
+
+#include <iostream>
+
+#include "ArucoDetection.h"
+
+
+
+int main()
+{
+	std::cout << "vhsdfj";
+	return 0;
+}
