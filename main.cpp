d8ee57a510c87abcbb2c659b73669520d0291cce
Rahul Kedia <kediarahul1011@gmail.com>
2020-07-15 18:23:22 +0530

    Add code to read video and detect markers in it.

--- main.cpp
+++ main.cpp
@@ -6,14 +6,46 @@
  */
 
 
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
 #include "ArucoDetection.h"
 
 
+std::string InputVideoPath = "Videos/Aruco_Hand_Video.mp4";
 
 int main()
 {
-	std::cout << "vhsdfj";
+	cv::VideoCapture Cap(InputVideoPath);
+
+	// Check if camera opened successfully
+	if (!Cap.isOpened()) 
+	{
+		std::cout << "Error opening video." << std::endl;
+		return -1;
+	}
+
+	while (1)
+	{
+		//Reading a frame
+		cv::Mat Frame;
+		Cap >> Frame;
+
+		// Breaking if frame is empty.
+		if (Frame.empty())
+			break;
+		
+		std::vector<int> IDs; std::vector<std::vector<cv::Point2f>> Corners;
+		DetectAruco(Frame, IDs, Corners);
+
+		cv::imshow("Input", Frame);
+		if (cv::waitKey(1) == 32)
+			break;
+	}
+
+	// Releasing and closing everything after process is completed.
+	Cap.release();
+	cv::destroyAllWindows();
+
 	return 0;
 }
